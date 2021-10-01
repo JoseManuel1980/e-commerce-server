34fcd1a3e130267418f4df9df72dc79011c1794d
Jose Palacio <jose_palacio@gmail.com>
2021-10-01 22:58:23 +0200

    first commit

---
+++ api/order/controllers/order.js
@@ -0,0 +1,56 @@
+"use strict";
+const stripe = require("stripe")(
+  "sk_test_51Je3wBCGCuCwkLoSpwSvRZgwGZLeKkvEfjQ7q0hQjyIB69Gz6FfJZcRA6X5qagc3O5nlxc404SBOdhLjhpBvB2ev00gkwIizjN"
+);
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async create(ctx) {
+    const { tokenStripe, products, idUser, addressShipping } = ctx.request.body;
+    let totalPayment = 0;
+
+    products.forEach((product) => {
+      const calcPrice = (price, discount) => {
+        if (!discount) return price;
+
+        const discountAmount = (price * discount) / 100;
+        return (price - discountAmount).toFixed(2);
+      };
+      totalPayment +=
+        calcPrice(product.price, product.discount) * product.quantity;
+    });
+
+    const charge = await stripe.charges.create({
+      amount: totalPayment * 100,
+      currency: "eur",
+      source: tokenStripe,
+      description: `ID Usuario: ${idUser}`,
+    });
+
+    const createOrder = [];
+    for await (const product of products) {
+      const data = {
+        product: product.id,
+        user: idUser,
+        totalPayment: totalPayment,
+        productsPayment: product.price * product.quantity,
+        quantity: product.quantity,
+        idPayment: charge.id,
+        addressShipping,
+      };
+
+      const validData = await strapi.entityValidator.validateEntityCreation(
+        strapi.models.order,
+        data
+      );
+      const entry = await strapi.query("order").create(validData);
+      createOrder.push(entry);
+    }
+
+    return createOrder;
+  },
+};
